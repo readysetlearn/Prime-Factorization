767464875e01d66847da8728a4021d14b7e48cef
readysetlearn <mister_josh@ymail.com>
2016-03-31 01:19:51 -0700

    Implemented factoring method which when used with prime test method, prime factors an integer.

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -5,18 +5,30 @@ public class PrimeFactorization {
 
 
     public static void main(String[] args) {
-        int[] test = {900937, 541580968};
-        for(int i : test) {
-            System.out.print(i+" is ");
-            if(isPrime(i)) {
-                System.out.println("prime");
+        int test = 91885987;
+        factor(test);
+    }
+    
+    /*factor input into its prime compisition*/
+    public static void factor(int input) {
+        while(input %2 == 0) {//since 2 is factored out at this step, consider removing the check for it in isPrime()
+            input /= 2;
+            System.out.println("2");
+        }
+        
+        for(int i = 3; i <= Math.floor(Math.sqrt(input)); i += 2) {
+            while(input % i == 0) {
+                input /= i;
+                System.out.println(i);
             }
-            else {
-                System.out.println("not prime");
+            if(isPrime(input)) {
+                System.out.println(input);
+                break;
             }
         }
     }
     
+    /*return true iff input is prime*/
     public static boolean isPrime(int input) {
         if(input == 2) {//2 is a special case
             return true;
