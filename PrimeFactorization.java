18a9395ae4c2edfa70f5329e875671ef49b0baf5
readysetlearn <mister_josh@ymail.com>
2016-03-31 00:11:58 -0700

    Implemented method that tests if integer is prime by checking odd numbers up to its square root (and 2).

---
+++ PrimeFactorization.java
@@ -0,0 +1,39 @@
+package primefactorization;
+
+
+public class PrimeFactorization {
+
+
+    public static void main(String[] args) {
+        int[] test = {900937, 541580968};
+        for(int i : test) {
+            System.out.print(i+" is ");
+            if(isPrime(i)) {
+                System.out.println("prime");
+            }
+            else {
+                System.out.println("not prime");
+            }
+        }
+    }
+    
+    public static boolean isPrime(int input) {
+        if(input == 2) {//2 is a special case
+            return true;
+        }
+        
+        if(input % 2 == 0) {
+            return false;
+        }
+        
+        //System.out.println("Math.floor(Math.sqrt(input)): "+Math.floor(Math.sqrt(input)));
+        for(int i = 3; i <= Math.floor(Math.sqrt(input)); i += 2) {       
+            if(input % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+}
