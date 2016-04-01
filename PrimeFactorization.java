15c2c27fd4cf510c4dd13ea4470f0e2244ee91a2
readysetlearn <mister_josh@ymail.com>
2016-04-01 04:18:36 -0700

    Returned ArrayList of Integers instead of directly displaying result on screen.

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -1,31 +1,38 @@
 package primefactorization;
 
+import java.util.ArrayList;
+
 
 public class PrimeFactorization {
 
 
     public static void main(String[] args) {
-        int test = 91885987;
-        factor(test);
+        int test = 1001;
+        ArrayList<Integer> factors = factor(test);
+        for(Integer i : factors) {
+            System.out.println(i);
+        }
     }
     
     /*factor input into its prime compisition*/
-    public static void factor(int input) {
+    public static ArrayList<Integer> factor(int input) {
+        ArrayList<Integer> factors = new ArrayList<Integer>();
         while(input %2 == 0) {//since 2 is factored out at this step, consider removing the check for it in isPrime()
             input /= 2;
-            System.out.println("2");
+            factors.add(2);
         }
         
         for(int i = 3; i <= Math.floor(Math.sqrt(input)); i += 2) {
             while(input % i == 0) {
                 input /= i;
-                System.out.println(i);
+                factors.add(i);
             }
             if(isPrime(input)) {
-                System.out.println(input);
-                break;
+                factors.add(input);//remainder is prime so we're done
+                return factors;
             }
         }
+        return null;
     }
     
     /*return true iff input is prime*/
